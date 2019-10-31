667a99b73a0b1d26beb5a4b1376c498eb9666374
Francesco Fico <fficusbenjamin@gmail.com>
2019-10-31 14:58:06 +0000

    cave class created
    cave class creted

---
+++ AICoursework/Cave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AICoursework
+{
+    class Cave
+    {
+        public int xCoord, yCoord;
+
+
+
+    }
+}
--- AICoursework/Program.cs
+++ AICoursework/Program.cs
@@ -10,6 +10,30 @@ namespace AICoursework
     {
         static void Main(string[] args)
         {
+
+            List<Cave> caves = new List<Cave>();
+
+
+            string inputFile = System.IO.File.ReadAllText(@"C:\Users\Francesco\Desktop\AI Courserwork\caverns files\input1.cav");
+            
+
+            string[] coord = inputFile.Split(',');
+            int firstNum = Convert.ToInt32(coord[0]);
+
+
+            for (int i=1; i<=firstNum*2;i++) 
+            {
+                Cave cave= new Cave();
+
+                cave.xCoord = Convert.ToInt32(coord[i]);
+                i++;
+                cave.yCoord = Convert.ToInt32(coord[i]);
+
+                System.Console.WriteLine(cave.xCoord+", " + cave.yCoord);
+            }
+            
+            System.Console.ReadKey();
+
         }
     }
 }
